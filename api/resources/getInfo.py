5c862b2bf22f186663f63097a9e9612e738e972c
Joshua Astray <JoshuaSRKF@gmail.com>
2019-05-10 03:19:23 +0800

    refactor: utils

--- api/resources/getInfo.py
+++ api/resources/getInfo.py
@@ -1,9 +1,26 @@
-from flask import session
+from flask import request, Response, make_response, session
 from flask_restful import Resource
 from api.common.database import database
+import os
+import time
+import phpserialize
 
 class getInfo(Resource):
 	def get(self):
+		if "open_id" not in session:
+			session_content = ""
+			if "session_id" in request.cookies:
+				session_file = "%ssess_%s" % ("/var/tmp/", request.cookies.get("session_id"))
+				if os.path.exists(session_file):
+					f = open(session_file)
+					session_content = f.read()
+			if session_content:
+				session_list = session_content.split("|")
+				if session_list[0]:
+					ret = phpserialize.loads(session_list[1])
+					if "open_id" in ret:
+						session["open_id"] = user_info["open_id"]
+
 		if "open_id" not in session:
 			return {
 				"ok": False,
