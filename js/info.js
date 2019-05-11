85e682686c5e29caacbc28d0f82e29d11235b3e1
ptongptong <45759756+ptongptong@users.noreply.github.com>
2019-05-12 06:57:22 +0800

    fix: info all

--- js/info.js
+++ js/info.js
@@ -33,6 +33,14 @@ submit.addEventListener("click",function(){
             dataType:"json",
             success:function(){
                 window.location.href="content.html";
+            },
+            fail:function(err){
+                if (err.status_code == 401) {
+                    location.href="#BBT微信后台#/Home/Index/index?state="+encodeURIComponent( location.href );
+                }
+                if (err.status_code == 409) {
+                    showError("用户名已存在");
+                }
             }
         })
     }
