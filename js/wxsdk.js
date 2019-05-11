2f4e0e90fd126916f8959d11e30545768288312f
ptongptong <45759756+ptongptong@users.noreply.github.com>
2019-05-12 06:39:07 +0800

    Update wxsdk.js

--- js/wxsdk.js
+++ js/wxsdk.js
@@ -1,44 +1,17 @@
 var prefix="http://server.sforest.in:2019/api/";
-var status=0;
-function bindwx(){
-    $.ajax({
-        url:prefix+"setSession",
-        type:"get",
-            ///////////////////////// TEST
-            xhrFields: {
-                withCredentials: true
-            },
-            crossDomain: true,
-            //////////////////////////
-        dataType:"json",
-        success:function(data){
-            wx.config({
-                debug:false, 
-                appId:data.appId, 
-                timestamp: data.timestamp, 
-                nonceStr: data.noncestr, 
-                signature: data.signature,
-                jsApiList: ['startRecord','stopRecord','onVoiceRecordEnd','onVoicePlayEnd','playVoice','pauseVoice','uploadVoice'] 
-            });
-            wx.ready(function(){
-                //接口调用成功
-            })
-        }
-    })
-}
+//绑定微信
 $.ajax({
     url:prefix+"getInfo",
     type:"get",
     dataType:"json",
     success:function(data){
-        if (!data.record){
-            bindwx();
-            status=1;
-        }
-         else{status=1;}
+        if (!data.record) {window.location.href="info.html";}
+         else {window.location.href="content.html";}
     },
-    error:function(data){
-        console.log(data);
+    fail: function(err) {
+        if (err.status_code == 401) {
+            location.href="#BBT微信后台#/Home/Index/index?state="+encodeURIComponent( location.href );
+        }
     }
 })
 
