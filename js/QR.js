4bd44901a33b08bfac981d967f4f568984628649
ptongptong <45759756+ptongptong@users.noreply.github.com>
2019-05-12 08:59:41 +0800

    update myQR

--- js/QR.js
+++ js/QR.js
@@ -1,14 +1,16 @@
 $.ajax({
-    url:"http://server.sforest.in:2019/api/getQRCode",
+    url:prefix+"getQRCode",
     type:"get",
     dateType:"json",
-    ///////////////////////// TEST
-    xhrFields: {
-        withCredentials: true
-    },
-    crossDomain: true,
-    //////////////////////////
     success:function(data){
         document.getElementById("share").src=data.image;
+    },
+    fail:function(err){
+        if (err.status_code == 401) {
+            location.href="#BBT微信后台#/Home/Index/index?state="+encodeURIComponent( location.href );
+        }
+        if (err.status_code == 403) {
+            location.href="info.html";
+        }
     }
 })
