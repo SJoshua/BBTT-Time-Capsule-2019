f4c6a812d6593b78ca2f7f891c5ac6e12c63ca3d
ptongptong <45759756+ptongptong@users.noreply.github.com>
2019-05-12 09:45:41 +0800

    update conclusion

--- js/conclusion.js
+++ js/conclusion.js
@@ -2,35 +2,39 @@ var winWidth = $(window).width();
 var winHeight = $(window).height();
 document.getElementById("background").style.backgroundSize=winWidth+'px '+winHeight+'px';
 $.ajax({
-    // url:"getInfo",
-    url:"http://server.sforest.in:2019/api/getInfo",
+    url:prefix+"getInfo",
+    // url:"./json/test.JSON",
     type:"get",
     dataType:"json",
-    ///////////////////////// TEST
-    xhrFields: {
-        withCredentials: true
-    },
-    crossDomain: true,
-    //////////////////////////
     success:function(data){
-        document.getElementById("who").innerHTML='<strong>'+data.name+'</strong>'+'  同学';
+        if (data.record==true) {
+             document.getElementById("who").innerHTML='<strong>'+data.name+'</strong>'+'  同学';
+        }
+         else {window.location.href="info.html"}
+    },
+    fail:function(err){
+        if (err.status_code == 401) {
+            location.href="#BBT微信后台#/Home/Index/index?state="+encodeURIComponent( location.href );
+        }
     }
 });
 $.ajax({
-    // url:"getStatistics",
-    url:"http://server.sforest.in:2019/api/getStatistics",
+    url:prefix+"getStatistics",
+    // url:"./json/test2.JSON",
     type:"get",
-    ///////////////////////// TEST
-    xhrFields: {
-        withCredentials: true
-    },
-    crossDomain: true,
-    //////////////////////////
     dataType:"json",
     success:function(data){
         document.getElementById("sent").innerHTML='<strong>'+data.sent+'</strong>';
         document.getElementById("received_by_qrcode").innerHTML='<strong>'+data.received_by_qrcode+'</strong>';
         document.getElementById("received_by_tel").innerHTML='<strong>'+data.received_by_tel+'</strong>';
         document.getElementById("answered").innerHTML='<strong>'+data.answered+'</strong>';
+    },
+    fail:function(err){
+        if (err.status_code == 401) {
+            location.href="#BBT微信后台#/Home/Index/index?state="+encodeURIComponent( location.href );
+        }
+        if (err.status_code == 403) {
+            location.href="info.html";
+        }
     }
 });
