2b17278053e081fc469a3cb700d8791759cb853a
ptongptong <45759756+ptongptong@users.noreply.github.com>
2019-05-15 00:04:07 +0800

    Update content.js

--- js/content.js
+++ js/content.js
@@ -1,7 +1,7 @@
-var t1=window.setTimeout(pop1,800);
-var t2=window.setTimeout(pop2,1600);
-var t3=window.setTimeout(pop3,2400);
-var t4=window.setTimeout(pop4,3200);
+var t1=window.setTimeout(pop1,600);
+var t2=window.setTimeout(pop2,1200);
+var t3=window.setTimeout(pop3,1800);
+var t4=window.setTimeout(pop4,2400);
 function pop1(){
     var obj= document.getElementById("1");
     obj.style.animation="pop 2s";
