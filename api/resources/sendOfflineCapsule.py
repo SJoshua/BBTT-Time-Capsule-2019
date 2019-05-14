1424b2760b804a45485c39ebc116bfecfe7b8195
Joshua Astray <joshua@moefactory.com>
2019-05-14 04:08:00 +0000

    revert: improper revert

--- api/resources/sendOfflineCapsule.py
+++ api/resources/sendOfflineCapsule.py
@@ -1,7 +1,7 @@
 from flask import session, request
 from flask_restful import Resource, reqparse, inputs, abort
 from api.common.database import database
-from api.common.utils import checkTag
+from api.common.utils import checkTag, checkTime
 import json
 import requests
 
@@ -32,6 +32,8 @@ parser.add_argument('seal', type = inputs.boolean, required = True)
 
 class sendOfflineCapsule(Resource):
 	def post(self):
+		if checkTime() != 0:
+			abort(416, message = "Event is not ongoing.")
 		if "open_id" not in session:
 			sess_id = request.cookies.get("PHPSESSID")
 			if sess_id is not None:
