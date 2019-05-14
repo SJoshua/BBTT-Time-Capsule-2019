afa876fb604662033428ab28400c8f03fa0278af
ptongptong <45759756+ptongptong@users.noreply.github.com>
2019-05-14 15:58:14 +0800

    getInfo

--- js/time-end.js
+++ js/time-end.js
@@ -1,4 +1,26 @@
-document.getElementById("who").innerHTML='<strong>'+localStorage.getItem('username')+'</strong>'+" 同学";
+$.ajax({
+    url:prefix+"getInfo",
+    // url:"./json/test.JSON",
+                                ///////////////////////// TEST
+                                xhrFields: {
+                                    withCredentials: true
+                                },
+                                crossDomain: true,
+                                //////////////////////////
+    type:"get",
+    dataType:"json",
+    success:function(data){
+        if (data.record==true) {
+             document.getElementById("who").innerHTML='<strong>'+data.name+'</strong>'+'  同学';
+        }
+         else {window.location.href="info.html"}
+    },
+    error:function(err){
+        if (err.status_code == 401) {
+            location.href=bbt+encodeURIComponent( location.href );
+        }
+    }
+});
 document.getElementById("count").innerHTML='<strong>'+localStorage.getItem('count')+'</strong>';
 document.getElementById("time").innerHTML='<strong>'+localStorage.getItem('time')+'</strong>';
 
